0fe963d654dd710fa96a4480c720322de1ce25dd
westerhack <westerhack@gmail.com>
2016-03-29 12:23:10 -0700

    made my own float class

--- objs/numbers/floatobj.py
+++ objs/numbers/floatobj.py
@@ -2,5 +2,9 @@ from objs import numobj, pyobj
 class floatobj(numobj, pyobj):
     """ A floating point number. """
     _regex = r'\b(\d+)?\.(?(1)\d*|\d+)[fF]?\b'
-    _pyobj = float
+    # _pyobj = float
+    class _float(float):
+        def __div__(self, other): return self / other
+        def __rdiv__(self, other): return other / self
+    _pyobj = _float
     _pyobj_rank = 2
