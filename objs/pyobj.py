fbd120dfd4dae358989a3156f9a1dc6e1f35f00c
westerhack <westerhack@gmail.com>
2016-03-29 09:58:41 -0700

    STR + OTHER works

--- objs/pyobj.py
+++ objs/pyobj.py
@@ -1,4 +1,3 @@
-from node import node
 from objs import obj
 class pyobj(obj):
     _pyobj = None
@@ -10,7 +9,7 @@ class pyobj(obj):
                 assert hasattr(node1, 'obj'), 'every object should!'
                 assert hasattr(node1.obj, '_pyobj'), "The Node's object should have a python object associated with it!"
             nodeobj = node1.obj if node1.obj._pyobj_rank >= node2.obj._pyobj_rank else node2.obj
-            return node.new(data = getattr(nodeobj._pyobj(node1.data), attr)(nodeobj._pyobj(node2.data)),
+            return node1.new(data = getattr(nodeobj._pyobj(node1.data), attr)(nodeobj._pyobj(node2.data)),
                         obj = nodeobj)
         return ret
     # def __add__(self, a, b):
--- objs/strobj.py
+++ objs/strobj.py
@@ -5,6 +5,10 @@ class strobj(pyobj, obj):
     _pyobj = str
     _pyobj_rank = 4
     def __add__(self, left, right, knowndict):
-        if left.data[-1] in knowndict.consts.punc.quotes:
-            pass
+        ldata, rdata = str(left.data), str(right.data)
+        if ldata[-1] in knowndict.consts.punc.quotes:
+            data = ldata[:-1] + rdata + ldata[-1]
+        else:
+            data = ldata + rdata
+        return left.new(data = data, obj = strobj)
         # if args[0][-1] in args[0].control
