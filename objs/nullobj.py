21f5002c44d89ce87efdae9b3fa8c7526a3826d3
westerhack <westerhack@gmail.com>
2016-04-18 11:46:09 -0700

    initial commit. nothing is here

--- objs/nullobj.py
+++
@@ -1,4 +0,0 @@
-from objs import obj, regexobj
-class nullobj(regexobj, obj):
-    """ A null object - usually gotten by the keywords 'nil', 'null', and 'None'. """
-    _regex = r'[nN](?:one|il|ull)'
